a1fdc96bd5ae1be11756a8056d4f1ba3813ba20c
JEANNOEL <nfonjeannoel1@gmail.com>
2021-12-28 11:57:17 -1100

    updated

--- main.py
+++ main.py
@@ -1,66 +1,78 @@
 import datetime
+import os
+import pathlib
 from random import randint
 
 import boto3
 import requests
 import json
+import datetime
 
 cities_urls = [
-
     # urls for all the cities in Australia
-    "https://www.ubereats.com/au/city/canberra-act",
-    "https://www.ubereats.com/au/city/byron-bay-nsw",
-    "https://www.ubereats.com/au/city/galston-nsw",
-    "https://www.ubereats.com/au/city/queanbeyan-nsw",
+    # "https://www.ubereats.com/au/city/canberra-act",
+    # "https://www.ubereats.com/au/city/byron-bay-nsw",
+    # "https://www.ubereats.com/au/city/galston-nsw",
+    # "https://www.ubereats.com/au/city/queanbeyan-nsw",
     "https://www.ubereats.com/au/city/wagga-wagga-nsw",
-    "https://www.ubereats.com/au/city/catherine-field-nsw",
-    "https://www.ubereats.com/au/city/central-coast-nsw",
-    "https://www.ubereats.com/au/city/bathurst-nsw",
-    "https://www.ubereats.com/au/city/wollongong-nsw",
-    "https://www.ubereats.com/au/city/leppington-nsw",
-    "https://www.ubereats.com/au/city/sydney-nsw",
-    "https://www.ubereats.com/au/city/newcastle-nsw",
-    "https://www.ubereats.com/au/city/tweed-heads-nsw",
-    "https://www.ubereats.com/au/city/old-bar-nsw",
-    "https://www.ubereats.com/au/city/wagga-wagga-nsw",
-    "https://www.ubereats.com/au/city/darwin-nt",
-    "https://www.ubereats.com/au/city/brisbane-qld",
-    "https://www.ubereats.com/au/city/cairns-qld",
-    "https://www.ubereats.com/au/city/gold-coast-qld",
-    "https://www.ubereats.com/au/city/hervey-bay-qld",
-    "https://www.ubereats.com/au/city/highfields-qld",
-    "https://www.ubereats.com/au/city/mackay-qld",
-    "https://www.ubereats.com/au/city/mount-cotton-qld",
-    "https://www.ubereats.com/au/city/rockhampton-qld",
-    "https://www.ubereats.com/au/city/sunshine-coast-qld",
-    "https://www.ubereats.com/au/city/toowoomba-qld",
-    "https://www.ubereats.com/au/city/townsville-qld",
-    "https://www.ubereats.com/au/city/tweed-heads-qld",
-    "https://www.ubereats.com/au/city/withcott-qld",
-    "https://www.ubereats.com/au/city/adelaide-sa",
-    "https://www.ubereats.com/au/city/angle-vale-sa",
-    "https://www.ubereats.com/au/city/gawler-sa",
-    "https://www.ubereats.com/au/city/hobart-tas",
-    "https://www.ubereats.com/au/city/launceston-tas",
-    "https://www.ubereats.com/au/city/ballarat-vic",
-    "https://www.ubereats.com/au/city/bendigo-vic",
-    "https://www.ubereats.com/au/city/diggers-rest-vic",
-    "https://www.ubereats.com/au/city/geelong-vic",
-    "https://www.ubereats.com/au/city/lara-vic",
-    "https://www.ubereats.com/au/city/leopold-vic",
-    "https://www.ubereats.com/au/city/melbourne-vic",
-    "https://www.ubereats.com/au/city/melton-vic",
-    "https://www.ubereats.com/au/city/officer-vic",
-    "https://www.ubereats.com/au/city/pakenham-vic",
-    "https://www.ubereats.com/au/city/rockbank-vic",
-    "https://www.ubereats.com/au/city/sunbury-vic",
-    "https://www.ubereats.com/au/city/wonga-park-vic",
-    "https://www.ubereats.com/au/city/baldivis-wa",
-    "https://www.ubereats.com/au/city/bunbury-wa",
-    "https://www.ubereats.com/au/city/ellenbrook-wa",
-    "https://www.ubereats.com/au/city/perth-wa",
-    "https://www.ubereats.com/au/city/yanchep-wa"
+    # "https://www.ubereats.com/au/city/catherine-field-nsw",
+    # "https://www.ubereats.com/au/city/central-coast-nsw",
+    # "https://www.ubereats.com/au/city/bathurst-nsw",
+    # "https://www.ubereats.com/au/city/wollongong-nsw",
+    # "https://www.ubereats.com/au/city/leppington-nsw",
+    # "https://www.ubereats.com/au/city/sydney-nsw",
+    # "https://www.ubereats.com/au/city/newcastle-nsw",
+    # "https://www.ubereats.com/au/city/tweed-heads-nsw",
+    # "https://www.ubereats.com/au/city/old-bar-nsw",
+    # "https://www.ubereats.com/au/city/wagga-wagga-nsw",
+    # "https://www.ubereats.com/au/city/darwin-nt",
+    # "https://www.ubereats.com/au/city/brisbane-qld",
+    # "https://www.ubereats.com/au/city/cairns-qld",
+    # "https://www.ubereats.com/au/city/gold-coast-qld",
+    # "https://www.ubereats.com/au/city/hervey-bay-qld",
+    # "https://www.ubereats.com/au/city/highfields-qld",
+    # "https://www.ubereats.com/au/city/mackay-qld",
+    # "https://www.ubereats.com/au/city/mount-cotton-qld",
+    # "https://www.ubereats.com/au/city/rockhampton-qld",
+    # "https://www.ubereats.com/au/city/sunshine-coast-qld",
+    # "https://www.ubereats.com/au/city/toowoomba-qld",
+    # "https://www.ubereats.com/au/city/townsville-qld",
+    # "https://www.ubereats.com/au/city/tweed-heads-qld",
+    # "https://www.ubereats.com/au/city/withcott-qld",
+    # "https://www.ubereats.com/au/city/adelaide-sa",
+    # "https://www.ubereats.com/au/city/angle-vale-sa",
+    # "https://www.ubereats.com/au/city/gawler-sa",
+    # "https://www.ubereats.com/au/city/hobart-tas",
+    # "https://www.ubereats.com/au/city/launceston-tas",
+    # "https://www.ubereats.com/au/city/ballarat-vic",
+    # "https://www.ubereats.com/au/city/bendigo-vic",
+    # "https://www.ubereats.com/au/city/diggers-rest-vic",
+    # "https://www.ubereats.com/au/city/geelong-vic",
+    # "https://www.ubereats.com/au/city/lara-vic",
+    # "https://www.ubereats.com/au/city/leopold-vic",
+    # "https://www.ubereats.com/au/city/melbourne-vic",
+    # "https://www.ubereats.com/au/city/melton-vic",
+    # "https://www.ubereats.com/au/city/officer-vic",
+    # "https://www.ubereats.com/au/city/pakenham-vic",
+    # "https://www.ubereats.com/au/city/rockbank-vic",
+    # "https://www.ubereats.com/au/city/sunbury-vic",
+    # "https://www.ubereats.com/au/city/wonga-park-vic",
+    # "https://www.ubereats.com/au/city/baldivis-wa",
+    # "https://www.ubereats.com/au/city/bunbury-wa",
+    # "https://www.ubereats.com/au/city/ellenbrook-wa",
+    # "https://www.ubereats.com/au/city/perth-wa",
+    # "https://www.ubereats.com/au/city/yanchep-wa"
 ]
+locale_code = 'au'
+city_name = "australia"
+
+
+def get_week():
+    return str(datetime.datetime.utcnow().isocalendar()[0]) + '-' + str(datetime.datetime.utcnow().isocalendar()[1])
+
+
+week = ""
+site_name = "ubereats"
 
 
 def get_json_stores(pathname):
@@ -83,14 +95,25 @@ def get_json_stores(pathname):
         'x-csrf-token': 'x',
     }
 
-    response = requests.request("POST", api_url, headers=headers, data=payload)
-
-    return json.loads(response.text)
+    for i in range(5):
+        response = requests.request(method="POST", url=api_url, headers=headers, data=payload)
+        # print(response.text)
+        response = json.loads(response.text)
+        # title = response['data'].get('title', None)
+        # if response['status'] == "failure":
+        #     print(f"failed to get data, retrying {i + 1} of 5 times")
+        # else:
+        #     if title is None:
+        #         continue
+        #     else:
+        #         break
+    return response
 
 
 def get_store(uuid):
-    api_url = "https://www.ubereats.com/api/getStoreV1?localeCode=au"
-    d = {"storeUuid": uuid, "sfNuggetCount": 6}
+    api_url = f"https://www.ubereats.com/api/getStoreV1?localeCode={locale_code}"
+    d = {"storeUuid": uuid, "sfNuggetCount": 8}
+    # print(uuid)
     payload = json.dumps(d)
     headers = {
         'authority': 'www.ubereats.com',
@@ -106,92 +129,103 @@ def get_store(uuid):
         'origin': 'https://www.ubereats.com',
         'x-csrf-token': 'x',
     }
-    response = requests.request("POST", api_url, headers=headers, data=payload)
-    return json.loads(response.text)
+    for i in range(5):
+        response = requests.request(method="POST", url=api_url, headers=headers, data=payload)
+        # print(response.text)
+        response = json.loads(response.text)
+        # title = response['data'].get('title', None)
+        # if response['status'] == "failure":
+        #     print(f"failed to get data, retrying {i + 1} of 5 times")
+        # else:
+        #     if title is None:
+        #         continue
+        #     else:
+        #         break
+    return response
+
+
+def create_path_store(my_section_name):
+    global week
+    week = get_week()
+    parent_dir = str(pathlib.Path(__file__).parent.resolve()).replace("\\", "/") + f"/{site_name}/{city_name}/"
+    path = os.path.join(parent_dir, week)
+
+    try:
+        os.makedirs(path, exist_ok=True)
+        print("sub Directory '%s' created successfully" % my_section_name)
+
+    except OSError as error:
+        print("sub Directory '%s' can not be created" % my_section_name)
 
 
 def get_store_details(details):
     store_info = {}
-    try:
-        data = details['data']
-    except:
-        data = None
+    data = details.get('data', None)
     if data is not None:
-        try:
-            title = store_info["title"] = data['title']
-        except:
-            title = store_info["title"] = "NA"
+        title = store_info["title"] = data.get('title', "NA")
+        store_info["uuid"] = data.get('uuid', "NA")
+        store_info["cuisine"] = data.get("categories", [])
+        store_info["slug"] = data.get("slug", "NA")
+        store_info["images"] = data.get('heroImageUrls', [])
 
-        try:
-            store_info["uuid"] = data['uuid']
-        except:
-            store_info["uuid"] = "NA"
-
-        try:
-            store_info["cuisine"] = data['categories']
-        except:
-            store_info["cuisine"] = []
-
-        try:
-            store_info["slug"] = data['slug']
-        except:
-            store_info["slug"] = "NA"
-
-        try:
-            store_info["images"] = data['heroImageUrls']
-        except:
-            store_info["images"] = []
-        try:
-            store_info["location"] = data['location']['address']
-        except:
+        temp = data.get('location')
+        if temp is not None:
+            store_info["location"] = temp.get('address', 'NA')
+        else:
             store_info["location"] = "NA"
-        try:
-            store_info["currency"] = data['currencyCode']
-        except:
-            store_info["currency"] = "NA"
-        try:
-            store_info["rating"] = data['rating']['ratingValue']
-        except:
-            store_info["rating"] = "NA"
+
+        store_info["currency"] = data.get('currencyCode', "NA")
+        store_info["rating"] = data.get('rating', "NA")
+        store_info["phoneNumber"] = data.get('phoneNumber', "NA")
+        store_info["price_range"] = data.get('priceBucket', "NA")
 
         try:
-            store_info["phoneNumber"] = data['phoneNumber']
+            start_time = str(data["hours"][0]["sectionHours"][0]["startTime"])
         except:
-            store_info["phoneNumber"] = "NA"
-
+            start_time = ""
         try:
-            store_info["price_range"] = data['priceBucket']
+            end_time = str(data["hours"][0]["sectionHours"][0]["endTime"])
         except:
-            store_info["price_range"] = "NA"
-
-        try:
-
-            try:
-                start_time = str(data["hours"][0]["sectionHours"][0]["startTime"])
-            except:
-                start_time = ""
-            try:
-                end_time = str(data["hours"][0]["sectionHours"][0]["endTime"])
-            except:
-                end_time = ""
-
-            # add a : before the last but one number, returns the time as an integer
-            store_info["opening_hours"] = {
-                "start_time": start_time,
-                "end_time": end_time
-            }
-        except:
-            store_info["opening_hours"] = "NA"
-            # print(data["hours"][0]["sectionHours"][0]["startTime"])
-        try:
-            meal_category = data['sectionEntitiesMap']
-        except:
-            meal_category = None
-
+            end_time = ""
+        store_info["opening_hours"] = {
+            "start_time": start_time,
+            "end_time": end_time
+        }
+        meal_category = data.get('sectionEntitiesMap', {})
+        # print(meal_category)
         menu_info = []
-        if meal_category is not None:
+        if meal_category == {} or meal_category is None:
+            print("meal category is none, fetching catalog-section")
+            meal_category = data.get('catalogSectionsMap', {})
+            if meal_category == {} or meal_category is None:
+                print("no menu data for " + title)
+            else:
+                # print(meal_category)
+
+                for category in meal_category.values():
+                    for meal in category:
+                        try:
+                            menu = meal['payload']['standardItemsPayload']['catalogItems']  # returns list
+                        except:
+                            menu = []
+                        if not menu:
+                            print(f"menu for {title} is differny format")
+                        else:
+                            for a_meal in menu:
+                                menu_info.append({
+                                    "img_url": a_meal.get('imageUrl', "NA"),
+                                    "price": a_meal.get('price', "NA"),
+                                    "priceTagline": a_meal.get('priceTagline', "NA"),
+                                    "meal_name": a_meal.get('title', "NA"),
+                                    "meal_description": a_meal.get('itemDescription', "NA"),
+                                    "meal_type": "NA",
+                                    "meal_uuid": a_meal.get('uuid', "NA")
+                                })
+                # catalogSectionUUID
+        else:
             for category in meal_category.values():
                 for meal in category.values():
+                    # print(meal)
                     sub_section = data['subsectionsMap']
                     # subsection_list = sections_list['subsectionUuids']
                     meal_uuid = meal['uuid']
@@ -238,17 +272,23 @@ def get_store_details(details):
                         "meal_uuid": meal_uuid
                     })
 
-    store_final = {
-        title: {
-            "store_info": store_info,
-            "menu_info": menu_info
+        store_final = {
+            title: {
+                "store_info": store_info,
+                "menu_info": menu_info
+            }
         }
-    }
-    return store_final
+        return store_final
 
 
-def save_file_locally(save_store, file_name):
-    with open(f"{file_name.split('/')[-1]}.json", "w") as f:
+def save_file_locally(save_store, path):
+    for key, val in save_store.items():
+        file_name = val.get('store_info')
+        file_name = file_name.get("uuid", str(randint(1, 4000)))
+        break
+    save_location = str(pathlib.Path(__file__).parent.resolve()).replace("\\",
+                                                                         "/") + f"/{site_name}/{city_name}/{week}/{file_name}"
+    with open(f"{save_location}.json", "w") as f:
         f.write(json.dumps(save_store))
     f.close()
 
@@ -280,7 +320,8 @@ from time import sleep
 
 def process_store(city_url):
     path = get_path_url(city_url)
-    print(path)
+    # print(path)
+    create_path_store(path.split('/')[-1])
     json_stores = get_json_stores(path)
     if json_stores['status'] == "failure":
         print(f"store {path} not found")
@@ -293,26 +334,31 @@ def process_store(city_url):
             ind = ind_store
             break
     store_list = json_stores['data']['elements'][ind]['feedItems']
-    my_list = []
+    # my_list = []
+    # print(store_list)
     counter = 0
     for store in store_list:
         counter += 1
         # print(store['uuid'])
         # store = store_list[3]
         store_uuid = store['uuid']
+        # print(store_uuid)
         store_details = get_store(store_uuid)
         # print(store_details)
+        # return
         # for key, value in store_details.items():
         #     print(f"{key} -- {value}")
         # break
         this_store = get_store_details(store_details)
-        my_list.append(this_store)
+        # this_store['store_url'] = f"https://www.ubereats.com/{locale_code}/city/{path.split('/')[-1]}"
+        # save store
+        # my_list.append(this_store)
         # print(this_store)
-        print("added new store" + store_uuid)
+        # print("added new store" + store_uuid)
         # if counter == 2:
-    save_file(my_list, path)
-    # print(my_list)
-    print(f"saved file {path}")
+        save_file_locally(this_store, path.split('/')[-1])
+        # print(my_list)
+        print(f"saved file {store_uuid}")
     # if counter == 4:
     #     break
 
